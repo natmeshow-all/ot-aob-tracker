bca6f1a805c07d3bc1c730632f113fd3625d9927
natshow <natmeshow@gmail.com>
2026-04-23 10:48:45 +0700

    fix: allow inline scripts in CSP

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(helmet({
         directives: {
             defaultSrc: ["'self'"],
             scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+            scriptSrcAttr: ["'unsafe-inline'"],
             styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
             fontSrc: ["'self'", "https://fonts.gstatic.com"],
             imgSrc: ["'self'", "data:", "https://ui-avatars.com"],
